6d5dd56cbe87aadca46d7e18a17517b59fb617a4
tyama711 <tyama711@gmail.com>
2022-02-12 04:47:59 +0900

    add --quiet option.

--- compute_engine/load-stock-price-into-bigquery/startup-script.sh
+++ compute_engine/load-stock-price-into-bigquery/startup-script.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo apt install -y python3-pip
+sudo apt install -q -y python3-pip
 python3 -m pip install --quiet --user load-stock-price-into-bigquery
 ${HOME}/.local/bin/load-stock-price-into-bigquery
 
