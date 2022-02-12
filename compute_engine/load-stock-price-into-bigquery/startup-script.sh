f82fd776d2dec0a7fc5c411fdcd7acc811df873a
tyama711 <tyama711@gmail.com>
2022-02-12 21:44:36 +0900

    bugfix

--- compute_engine/load-stock-price-into-bigquery/startup-script.sh
+++ compute_engine/load-stock-price-into-bigquery/startup-script.sh
@@ -2,8 +2,7 @@
 sudo apt-get -qq -y update
 sudo apt-get -qq -y install python3-pip
 python3 -m pip install --quiet --user load-stock-price-into-bigquery
-env
-${HOME}/.local/bin/load-stock-price-into-bigquery
+/root/.local/bin/load-stock-price-into-bigquery
 
 # delete an instance itself
 export NAME=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/name -H 'Metadata-Flavor: Google')
